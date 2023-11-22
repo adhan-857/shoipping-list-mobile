4fcca03e3bc37bac4c586049fdc54b5facddd969
adhan-857 <adhan.andika857@gmail.com>
2023-11-22 13:50:27 +0700

    Coba tutorial 9

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/screens/list_product.dart';
 import 'package:shopping_list/screens/menu.dart';
 import 'package:shopping_list/screens/shoplist_form.dart';
-import 'package:shopping_list/screens/list_product.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -12,12 +12,10 @@ class LeftDrawer extends StatelessWidget {
       child: ListView(
         children: [
           const DrawerHeader(
-            // TODO: Bagian drawer header
-            decoration: BoxDecoration(
-              color: Colors.indigo,
-            ),
-            child: Column(
-              children: [
+              decoration: BoxDecoration(
+                color: Colors.indigo,
+              ),
+              child: Column(children: [
                 Text(
                   'Shopping List',
                   textAlign: TextAlign.center,
@@ -28,52 +26,50 @@ class LeftDrawer extends StatelessWidget {
                   ),
                 ),
                 Padding(padding: EdgeInsets.all(10)),
-                Text("Catat seluruh keperluan belanjamu di sini!",
-                    // TODO: Tambahkan gaya teks dengan center alignment, font ukuran 15, warna putih, dan weight biasa
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                    ),
+                Text(
+                  "Catat seluruh keperluan belanjamu di sini!",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
                 ),
-              ],
-            ),
-          ),
-          // TODO: Bagian routing
+              ])),
           ListTile(
             leading: const Icon(Icons.home_outlined),
-            title: const Text('Halaman Utama'),
-            // Bagian redirection ke MyHomePage
+            title: const Text("Halaman Utama"),
+            // redirection ke halaman utama
             onTap: () {
               Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => MyHomePage(),
-                  ));
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MyHomePage(),
+                ),
+              );
             },
           ),
           ListTile(
-            leading: const Icon(Icons.add_shopping_cart),
-            title: const Text('Tambah Produk'),
-            // Bagian redirection ke ShopFormPage
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const ShopFormPage(),
-                  ));
-            },
-          ),
-          ListTile(
-              leading: const Icon(Icons.shopping_basket),
-              title: const Text('Daftar Produk'),
+              leading: const Icon(Icons.add_shopping_cart),
+              title: const Text("Tambah Produk"),
+              // redirection ke ShopFormPage
               onTap: () {
-                  // Route menu ke halaman produk
-                  Navigator.push(
+                Navigator.pushReplacement(
                   context,
-                  MaterialPageRoute(builder: (context) => const ProductPage()),
-                  );
-              },
+                  MaterialPageRoute(
+                    builder: (context) => ShopFormPage(),
+                  ),
+                );
+              }),
+          ListTile(
+            leading: const Icon(Icons.shopping_basket),
+            title: const Text('Daftar Produk'),
+            onTap: () {
+              // Route menu ke halaman produk
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ProductPage()),
+              );
+            },
           ),
         ],
       ),
